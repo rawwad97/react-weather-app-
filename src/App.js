a56bfe6c8ad41611428bd951c10d13dead383f8f
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2023-01-12 09:56:19 -0600

    I added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"> Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://cool-florentine-1729b5.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rawan Awwad
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/rawwad97/react-weather-app-"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
