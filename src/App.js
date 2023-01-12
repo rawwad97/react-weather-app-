7d693553148f1936a4fd31ffab322c83cbc54e72
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2023-01-12 11:09:37 -0600

    I added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://cool-florentine-1729b5.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rawan Awwad
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/rawwad97/react-weather-app-"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://cool-florentine-1729b5.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rawan Awwad
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/rawwad97/react-weather-app-"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
