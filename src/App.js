c6ca4d33b7a2352754b2c3c7af5e7d8a8b54b9cb
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2023-01-12 12:08:46 -0600

    Created weather structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App </h1>
-        <button className="btn btn-primary">Hello</button>
+        <Weather />
         <footer>
           This project is coded by{" "}
           <a
