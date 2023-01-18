80f0178dc031e525713f606b44749b55aab0a05c
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2023-01-18 12:58:10 -0600

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather deafaultCity="New York" />
         <footer>
           This project is coded by{" "}
           <a
